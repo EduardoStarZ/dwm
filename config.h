b0447ab255e389a443113abb72b2caf9dd2a51f2
EduardoStarZ <pathver@gmail.com>
2025-05-28 12:41:51 -0300

    window rules

--- config.h
+++ config.h
@@ -34,8 +34,9 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "Discord",  NULL,		  NULL,       9,			0,			 -1 },
+	{ "zen",  NULL,       NULL,			  1 << 9,       0,           -1 },
+	{ "discord",  NULL,		  NULL,       1 << 2,		0,			 -1 },
+	{ "steam",	  NULL,		  NULL,		  1 << 10,		0,			 -1 },
 };
 
 /* layout(s) */
